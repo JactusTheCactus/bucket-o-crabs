af0dc9971afcb72564263eff0d00d7505e5cf384
Rusty <rusty@crabbucketing.crab>
2025-10-07 13:40:06 +0000

    Commit Changes

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,10 @@ fn main() {
 	println!("\tPress Ctrl+C to quit.");
 	loop {
 		crabs += generator.income_per_tick;
-		println!(
-			"The Bucket has {:.0} Crabs",
-			crabs
-		);
+		println!("The Bucket has {:.0} Crabs", crabs);
 		println!(
 			"\t{}:\n\t\t+{:.0}",
-			generator.name,
-			generator.income_per_tick
+			generator.name, generator.income_per_tick
 		);
 		thread::sleep(tick_duration);
 	}
